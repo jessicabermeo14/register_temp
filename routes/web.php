ad76c081955cab3dc8d912c8265ef4bac0501721
Jessica Bermeo <jessicabermeo14@gmail.com>
2021-01-27 16:11:04 -0500

    login en primera pantalla

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/home', [App\Http\Controllers\VisitorController::class, 'home'])->name('visitantes.home')->middleware('auth');
